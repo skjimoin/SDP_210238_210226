b8ab20a620303a3a1f43a0c339246a079ce689e4
uumoo <144724345+uumoo@users.noreply.github.com>
2024-02-05 14:10:21 +0600

    2nd commit
    signup page

--- main.py
+++ main.py
@@ -2,16 +2,47 @@ import tkinter as tk
 from tkinter import messagebox
 
 def show_signup_page():
-    printf("signup")
+    signup_window = tk.Toplevel(root)
+    signup_window.title("Journalify")
+    
+    tk.Label(signup_window, text="Sign Up").pack()
+
+    tk.Label(signup_window, text="Enter your Information").pack()
+
+    tk.Label(signup_window, text="Username:").pack()
+    signup_username_entry = tk.Entry(signup_window)
+    signup_username_entry.pack()
+
+    tk.Label(signup_window, text="Password:").pack()
+    signup_password_entry = tk.Entry(signup_window, show="*")
+    signup_password_entry.pack()
+
+    tk.Label(signup_window, text="Confirm Password:").pack()
+    confirm_password_entry = tk.Entry(signup_window, show="*")
+    confirm_password_entry.pack()
+
+    def perform_signup():
+        new_username = signup_username_entry.get()
+        new_password = signup_password_entry.get()
+        confirm_password = confirm_password_entry.get()
+
+        if new_password == confirm_password:
+            messagebox.showinfo("Signup", f"Signup successful for {new_username}!")
+            signup_window.destroy()
+        else:
+            messagebox.showerror("Signup Error", "Password doesn't match")
+
+    signup_button = tk.Button(signup_window, text="Sign Up", command=perform_signup)
+    signup_button.pack()
 
 def login():
     username = username_entry.get()
     password = password_entry.get()
 
     if username and password:
-        messagebox.showinfo("Login", "Login Done!")
+        messagebox.showinfo("Login", "Login Complete!")
     else:
-        messagebox.showerror("Login Error", "Invalid!!!!!!!! give username and password")
+        messagebox.showerror("Login Error", "Invalid! Please give username and password")
 
 root = tk.Tk()
 root.title("Login Page")
